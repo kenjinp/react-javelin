2c96af9ed535d8bbe7e447353831a9edb53127e2
Kenneth Pirman <github@kenny.wtf>
2022-01-21 23:21:07 +0100

    fix: optimize monitor component to improve fps, add deps argument to use-system hook

--- src/components/monitor/Monitor.tsx
+++ src/components/monitor/Monitor.tsx
@@ -1,27 +1,65 @@
-import { Query, World } from "@javelin/ecs";
+import { Query, useMonitor, World } from "@javelin/ecs";
+import { debounce, uniqueId } from "lodash";
 import * as React from "react";
-import { useMonitor } from "../../hooks/use-monitor/useMonitor";
 import { useWorld } from "../../hooks/use-world/useWorld";
 import { EntityContext } from "../../providers/entity-provider/EntityProvider";
 
 export interface MonitorProps {
   query: Query;
   children: (props: { entityId: number; world: World | null }) => React.ReactChild | React.ReactChild[] | null;
+  name?: string;
+  debounceMs?: number;
 }
 
-export const Monitor: React.FC<MonitorProps> = ({ children, query }) => {
-  const entities = useMonitor(query);
+export const Monitor: React.FC<MonitorProps> = ({ children, query, name = uniqueId(), debounceMs = 100 }) => {
+  const [matchingEntityIds] = React.useState(new Set<number>());
+  const valuesRef = React.useRef<number[]>([]);
+  const [updates, setUpdates] = React.useState(uniqueId());
   const world = useWorld();
-
-  return (
-    <>
-      {entities.map((entityId: number) => {
-        return (
-          <EntityContext.Provider value={entityId} key={entityId}>
-            {children({ entityId, world })}
-          </EntityContext.Provider>
-        );
-      })}
-    </>
+  const update = React.useRef(
+    debounce(() => {
+      valuesRef.current = Array.from(matchingEntityIds.values());
+      setUpdates(uniqueId());
+    }, debounceMs)
   );
+
+  React.useEffect(() => {
+    const system = () => {
+      useMonitor(
+        query,
+        (entityId: number) => {
+          matchingEntityIds.add(entityId);
+          update.current();
+        },
+        (entityId: number) => {
+          matchingEntityIds.delete(entityId);
+          update.current();
+        }
+      );
+    };
+    if (world) {
+      world.addSystem(system);
+    }
+    return () => {
+      if (world) {
+        world.removeSystem(system);
+      }
+    };
+  }, [world, matchingEntityIds, query]);
+
+  const entities = React.useMemo(() => {
+    return (
+      <React.Fragment key={name}>
+        {valuesRef.current.map((entityId: number) => {
+          return (
+            <EntityContext.Provider value={entityId} key={entityId}>
+              {children({ entityId, world })}
+            </EntityContext.Provider>
+          );
+        })}
+      </React.Fragment>
+    );
+  }, [updates, children, name, world]);
+
+  return entities;
 };
