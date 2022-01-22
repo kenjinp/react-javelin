3f9e21a4d743c5656d17e21c66343519f17d813c
Kenneth Pirman <github@kenny.wtf>
2022-01-22 15:08:10 +0100

    fix: replaces use-monitor hook that broke when trying to optimize (#11)

--- src/components/monitor/Monitor.tsx
+++ src/components/monitor/Monitor.tsx
@@ -1,5 +1,4 @@
 import { Query, World } from "@javelin/ecs";
-import { uniqueId } from "lodash";
 import * as React from "react";
 import { DEFAULT_MONITOR_DEBOUNCE_MS, useMonitor } from "../../hooks/use-monitor/useMonitor";
 import { useWorld } from "../../hooks/use-world/useWorld";
@@ -8,31 +7,22 @@ import { EntityContext } from "../../providers/entity-provider/EntityProvider";
 export interface MonitorProps {
   query: Query;
   children: (props: { entityId: number; world: World | null }) => React.ReactChild | React.ReactChild[] | null;
-  name?: string;
   debounceMs?: number;
 }
 
-export const Monitor: React.FC<MonitorProps> = ({
-  children,
-  query,
-  name = uniqueId(),
-  debounceMs = DEFAULT_MONITOR_DEBOUNCE_MS,
-}) => {
-  const entitiesIdList = useMonitor(query, name, debounceMs);
+export const Monitor: React.FC<MonitorProps> = ({ children, query, debounceMs = DEFAULT_MONITOR_DEBOUNCE_MS }) => {
+  const entitiesIdList = useMonitor(query, debounceMs);
   const world = useWorld();
-  const entities = React.useMemo(() => {
-    return (
-      <React.Fragment key={name}>
-        {entitiesIdList.map((entityId: number) => {
-          return (
-            <EntityContext.Provider value={entityId} key={entityId}>
-              {children({ entityId, world })}
-            </EntityContext.Provider>
-          );
-        })}
-      </React.Fragment>
-    );
-  }, [entitiesIdList, children, name, world]);
 
-  return entities;
+  return (
+    <React.Fragment>
+      {entitiesIdList.map((entityId: number) => {
+        return (
+          <EntityContext.Provider value={entityId} key={entityId}>
+            {children({ entityId, world })}
+          </EntityContext.Provider>
+        );
+      })}
+    </React.Fragment>
+  );
 };
